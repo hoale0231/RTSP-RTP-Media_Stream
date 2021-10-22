a873fc332863d6ab887950ccb13667308e1b18fa
kogleo <hellresiak@gmail.com>
2021-10-22 22:35:28 +0700

    Add RtpPacket

--- RtpPacket.py
+++ RtpPacket.py
@@ -22,22 +22,22 @@ class RtpPacket:
 		
 		# Get the payload from the argument
 		# self.payload = ...
-		header[0] = (header[0] | version << 6)&0xC0    				# 2 bits
-		header[0] = (header[0] | padding << 5) 				# 1 bit
-		header[0] = (header[0] | extension << 4)              # 1 bit
-		header[0] = (header[0] | (cc & 0x0F))                 # 4 bits
-		header[1] = (header[1] | marker << 7)                # 1 bit
-		header[1] = (header[1] | (pt & 0x7f))                 # 7 bits
-		header[2] = (seqnum & 0xFF00) >> 8                    # 16 bits total, this is first 8
-		header[3] = (seqnum & 0xFF)                          # second 8
-		header[4] = (timestamp >> 24)                          # 32 bit timestamp
+		header[0] = header[0] | version << 6
+		header[0] = header[0] | padding << 5 
+		header[0] = header[0] | extension << 4             # 1 bit
+		header[0] =	header[0] | cc                 # 4 bits
+		header[1] = header[1] | marker << 7              # 1 bit
+		header[1] = header[1] | pt                 # 7 bits
+		header[2] = seqnum >> 8                    # 16 bits total, this is first 8
+		header[3] = seqnum                          # second 8
+		header[4] = (timestamp >> 24) & 0xFF                        # 32 bit timestamp
 		header[5] = (timestamp >> 16) & 0xFF
 		header[6] = (timestamp >> 8) & 0xFF
 		header[7] = (timestamp & 0xFF)
-		header[8] = (ssrc >> 24)                             # 32 bit ssrc
-		header[9] = (ssrc >> 16) & 0xFF
-		header[10] = (ssrc >> 8) & 0xFF
-		header[11] = ssrc & 0xFF
+		header[8] = ssrc >> 24                             # 32 bit ssrc
+		header[9] = ssrc >> 16 
+		header[10] = ssrc >> 8
+		header[11] = ssrc 
 		self.header = header
         
 		#Get the payload from the argument
